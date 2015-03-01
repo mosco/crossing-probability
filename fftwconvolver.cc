d08002ecf82bf530d5527480da79262f8643bae4
Amit Moscovich Eiger <moscovich@gmail.com>
2015-03-01 19:19:18 +0200

    Make FFTWConvolver faster by initializing less objects.
    Construct FFTW_R2C_1D_Executor and FFTW_C2R_1D_Executor only for sizes
    that are a multiple of ROUNDING. Thus less FFTW plans have to be
    initialized.

--- fftwconvolver.cc
+++ fftwconvolver.cc
@@ -3,31 +3,35 @@
 #include "fftw_wrappers.hh"
 #include "fftwconvolver.hh"
 
+// Constructing FFTW_R2C_1D_Executor objects for every input size takes a lot of time, so we round up the sizes.
+const int ROUNDING = 128;
+
 FFTWConvolver::FFTWConvolver(int maximum_input_size) :
-    maximum_input_size(maximum_input_size),
-    r2c_executors(maximum_input_size+1, NULL),
-    c2r_executors(maximum_input_size+1, NULL),
-    tmp(maximum_input_size+1)
+    maximum_input_size(maximum_input_size+ROUNDING-1),
+    r2c_executors(maximum_input_size+ROUNDING, NULL),
+    c2r_executors(maximum_input_size+ROUNDING, NULL),
+    tmp(maximum_input_size+ROUNDING)
 {
 }
 
 void FFTWConvolver::convolve_same_size(int input_buffers_size, const double* input_buffer_0, const double* input_buffer_1, double* output_buffer)
 {
-    int padded_length = 2*input_buffers_size;
-    assert(input_buffers_size <= maximum_input_size);
-    if (r2c_executors[input_buffers_size] == NULL) {
-        //cerr << "Allocating " << input_buffers_size << endl;
-        assert(r2c_executors[input_buffers_size] == NULL);
-        assert(c2r_executors[input_buffers_size] == NULL);
-        r2c_executors[input_buffers_size] = new FFTW_R2C_1D_Executor(2*input_buffers_size);
-        c2r_executors[input_buffers_size] = new FFTW_C2R_1D_Executor(2*input_buffers_size);
+    int size_rounded = ROUNDING*((input_buffers_size+ROUNDING-1)/ROUNDING);
+    int padded_length = 2*size_rounded;
+    assert(size_rounded <= maximum_input_size);
+    if (r2c_executors[size_rounded/ROUNDING] == NULL) {
+        //cerr << "Allocating " << size_rounded << endl;
+        assert(r2c_executors[size_rounded/ROUNDING] == NULL);
+        assert(c2r_executors[size_rounded/ROUNDING] == NULL);
+        r2c_executors[size_rounded/ROUNDING] = new FFTW_R2C_1D_Executor(padded_length);
+        c2r_executors[size_rounded/ROUNDING] = new FFTW_C2R_1D_Executor(padded_length);
     }
-    //cerr << "accessing " << input_buffers_size << endl;
+    //cerr << "accessing " << size_rounded << endl;
 
-    FFTW_R2C_1D_Executor& fft = *r2c_executors[input_buffers_size];
+    FFTW_R2C_1D_Executor& fft = *r2c_executors[size_rounded/ROUNDING];
     assert (fft.input_size == padded_length);
 
-    FFTW_C2R_1D_Executor& ifft = *c2r_executors[input_buffers_size];
+    FFTW_C2R_1D_Executor& ifft = *c2r_executors[size_rounded/ROUNDING];
     assert (ifft.input_size == padded_length/2+1);
 
     fft.set_input_zeropadded(input_buffer_0, input_buffers_size);
