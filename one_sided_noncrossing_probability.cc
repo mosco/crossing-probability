956a35b3b0dc4e26c08ad0780b331eaeb002e526
Amit Moscovich Eiger <moscovich@gmail.com>
2015-03-04 01:52:57 +0200

    Add one-sided binomial crossing probability using translated polynomials.

--- one_sided_noncrossing_probability.cc
+++ one_sided_noncrossing_probability.cc
@@ -2,26 +2,50 @@
 #include <stdexcept>
 #include <cassert>
 #include <iostream>
-#include <quadmath.h>
-#include <math.h>
+#include <sstream>
 #include "one_sided_noncrossing_probability.hh"
 
 using namespace std;
 
-typedef __float128 FLOAT;
-#define EXP expq
-#define LOG logq
-#define LOG_GAMMA lgammaq
+#define USE_FLOAT128
 
-//typedef double FLOAT;
-//#define EXP exp
-//#define LOG log
-//#define LOG_GAMMA lgamma
+#ifdef USE_FLOAT128
+    typedef __float128 FLOAT;
+    #include <quadmath.h>
+    #define EXP expq
+    #define LOG logq
+    #define GAMMA tgammaq
+    #define LOG_GAMMA lgammaq
+    #define POW powq
+    #define TO_STRING float128_to_string
+    string float128_to_string(__float128 x)
+    {
+        static char s[1000];
+        quadmath_snprintf(s, sizeof(s), "%.30Qg", x);
+        return string(s);
+    }
+#else
+    typedef double FLOAT;
+    #include <math.h>
+    #define EXP exp
+    #define LOG log
+    #define GAMMA tgamma
+    #define LOG_GAMMA lgamma
+    #define POW pow
+    #define TO_STRING double_to_string
+#endif
+
+string double_to_string(double x)
+{
+    stringstream ss;
+    ss.precision(17); // http://stackoverflow.com/questions/554063/how-do-i-print-a-double-value-with-full-precision-using-cout
+    ss << x;
+    return ss.str();
+}
 
 class Polynomial {
 public:
     Polynomial(int max_degree);
-    FLOAT get_coefficient(int degree) const;
     void set_coefficient(int degree, FLOAT value);
     FLOAT evaluate(FLOAT x) const;
     void integrate();
@@ -37,15 +61,6 @@ Polynomial::Polynomial(int max_degree) :
     assert(max_degree >= 0);
 }
 
-FLOAT Polynomial::get_coefficient(int degree) const
-{
-    assert(degree >= 0);
-    assert(degree <= this->degree);
-
-    return coefficients[degree];
-
-}
-
 void Polynomial::set_coefficient(int degree, FLOAT value)
 {
     assert(degree >= 0);
@@ -56,11 +71,9 @@ void Polynomial::set_coefficient(int degree, FLOAT value)
 
 FLOAT Polynomial::evaluate(FLOAT x) const
 {
-    FLOAT x_pow_i = 1.0;
     FLOAT result = 0.0;
     for (int i = 0; i < degree+1; ++i) {
-        result += coefficients[i] * x_pow_i;
-        x_pow_i *= x;
+        result += coefficients[i] * POW(x, i);
     }
     return result;
 }
@@ -75,22 +88,82 @@ void Polynomial::integrate()
     ++degree;
 }
 
-//static void print_FLOAT_array(const FLOAT* arr, int n)
-//{
-//    for (int i = 0; i < n; ++i) {
-//        cout << arr[i] << ", ";
-//    }
-//    cout << endl;
-//}
+class PolynomialTranslatedMonomials {
+public:
+    PolynomialTranslatedMonomials(int max_degree);
+    void set_multiplicative_coefficient(int degree, FLOAT multiplicative_coefficient);
+    void set_additive_coefficient(int degree, FLOAT additive_coefficient);
+    FLOAT evaluate(FLOAT x) const;
+    void integrate();
+    friend ostream& operator<<(ostream& stream, const PolynomialTranslatedMonomials& poly);
+    vector<FLOAT> multiplicative_coefficients;
+    vector<FLOAT> additive_coefficients;
+    int degree;
+};
+
+PolynomialTranslatedMonomials::PolynomialTranslatedMonomials(int max_degree) :
+    multiplicative_coefficients(max_degree+1, 0.0),
+    additive_coefficients(max_degree+1, 0.0),
+    degree(0)
+{
+    assert(max_degree >= 0);
+}
+
+void PolynomialTranslatedMonomials::set_multiplicative_coefficient(int degree, FLOAT multiplicative_coefficient)
+{
+    assert(degree >= 0);
+    assert(degree <= this->degree);
+
+    multiplicative_coefficients[degree] = multiplicative_coefficient;
+}
+
+void PolynomialTranslatedMonomials::set_additive_coefficient(int degree, FLOAT additive_coefficient)
+{
+    assert(degree >= 0);
+    assert(degree <= this->degree);
+
+    additive_coefficients[degree] = additive_coefficient;
+}
+
+FLOAT PolynomialTranslatedMonomials::evaluate(FLOAT x) const
+{
+    FLOAT result = 0.0;
+    for (int i = 0; i < degree+1; ++i) {
+        result += multiplicative_coefficients[i] * POW(x+additive_coefficients[i], i);
+    }
+    return result;
+}
+
+void PolynomialTranslatedMonomials::integrate()
+{
+    assert(degree <= (int)multiplicative_coefficients.size());
+    for (int i = degree+1; i >= 1; --i) {
+        multiplicative_coefficients[i] = multiplicative_coefficients[i-1] / ((FLOAT)i);
+        additive_coefficients[i] = additive_coefficients[i-1];
+    }
+    additive_coefficients[1] = 0.0;
+    additive_coefficients[0] = 0.0;
+    multiplicative_coefficients[0] = 0.0;
+    ++degree;
+}
+
+static void print_FLOAT_array(const FLOAT* arr, int n)
+{
+    for (int i = 0; i < n; ++i) {
+        cout << TO_STRING(arr[i]) << ", ";
+    }
+    cout << endl;
+}
 
 ostream& operator<<(ostream& stream, const Polynomial& poly)
 {
     for (int i = poly.degree; i >= 0; --i) {
         FLOAT coef = poly.coefficients[i];
         if (coef >= 0) {
-            stream << (i != poly.degree ? " + " : "") << (double)coef;
+            stream << (i != poly.degree ? " + " : "") << TO_STRING(coef);
+            
         } else {
-            stream << (i != poly.degree ? " - " : "") << (double)-coef;
+            stream << (i != poly.degree ? " - " : "") << TO_STRING(-coef);
         }
         if (i >= 1) {
             stream << " x^" << i;
@@ -98,28 +171,114 @@ ostream& operator<<(ostream& stream, const Polynomial& poly)
     }
     return stream;
 }
+ostream& operator<<(ostream& stream, const PolynomialTranslatedMonomials& poly)
+{
+    for (int i = poly.degree; i >= 0; --i) {
+        FLOAT coef = poly.multiplicative_coefficients[i];
+        if (coef >= 0) {
+            stream << (i != poly.degree ? " + " : "") << TO_STRING(coef);
+            
+        } else {
+            stream << (i != poly.degree ? " - " : "") << TO_STRING(-coef);
+        }
+        if (i >= 1) {
+            stream << " (x + " << TO_STRING(poly.additive_coefficients[i]) << ")^" << i;
+        }
+    }
+    return stream;
+}
 
-double binomial_process_upper_noncrossing_probability(int n, const vector<double>& upper_bound_steps)
+double binomial_process_upper_noncrossing_probability_naive(int n, const vector<double>& upper_bound_steps)
 {
     if ((int)upper_bound_steps.size() < n) {
         throw runtime_error("Binomial process b(t) must cross upper boundary h(t) since h(1) < n and b(t) = n");
-        return 0;
     }
     Polynomial p(n);
     p.set_coefficient(0, 1.0);
-    for (int i = 0; i < (int)upper_bound_steps.size(); ++i) {
-        cout << "i == " << i << ": " << p << endl;
+    for (int i = 0; i < n; ++i) {
+        cout << "============= i == " << i << ": " << p << endl;
         //cout << "Before integration: ";
         //print_FLOAT_array(&p.coefficients[0], p.degree+1);
         p.integrate();
         //cout << "After integration: ";
         //print_FLOAT_array(&p.coefficients[0], p.degree+1);
         //cout << "After integration (<<): " << p << endl;
+        //cout << "Setting constant to -p.evaluate(" << (double)upper_bound_steps[i] << ") == " << (double)-p.evaluate(upper_bound_steps[i]) << endl;
         p.set_coefficient(0, -p.evaluate(upper_bound_steps[i]));
     }
     FLOAT integral_result = p.evaluate(1);
+    cout << "Integral result: " << TO_STRING(integral_result) << endl;
+    cout << "LOG_GAMMA(n+1) == " << TO_STRING(LOG_GAMMA(n+1)) << endl;
+    cout << "LOG(integral_result) == " << TO_STRING(LOG(integral_result)) << endl;
+    cout << "EXP(LOG_GAMMA(n+1) + LOG(integral_result))" << TO_STRING(EXP(LOG_GAMMA(n+1) + LOG(integral_result))) << endl;
     return EXP(LOG_GAMMA(n+1) + LOG(integral_result));
 }
+
+double binomial_process_upper_noncrossing_probability_lazy_integration(int n, const vector<double>& upper_bound_steps)
+{
+    if ((int)upper_bound_steps.size() < n) {
+        throw runtime_error("Binomial process b(t) must cross upper boundary h(t) since h(1) < n and b(t) = n");
+    }
+    vector<FLOAT> constant_terms(n+1, 0);
+    constant_terms[0] = 1.0;
+    for (int i = 0; i < n; ++i) {
+        FLOAT new_constant_term = 0;
+        for (int j = 0; j <=i; j++) {
+            new_constant_term -= constant_terms[j] * POW(upper_bound_steps[i], i-j+1) / GAMMA(i-j+2); // Li^{i-j+1}/(i-j+1)!
+        }
+        constant_terms[i+1] = new_constant_term;
+    }
+    cout << "constant_terms:\n";
+    print_FLOAT_array(&constant_terms[0], n+1);
+
+    FLOAT evaluation_at_1 = 0.0;
+    for (int i = 0; i < n+1; ++i) {
+        evaluation_at_1 += constant_terms[i] / GAMMA(n+1-i);
+    }
+    FLOAT integral_result = evaluation_at_1;
+    cout << "Integral result: " << TO_STRING(integral_result) << endl;
+    cout << "LOG_GAMMA(n+1) == " << TO_STRING(LOG_GAMMA(n+1)) << endl;
+    cout << "LOG(integral_result) == " << TO_STRING(LOG(integral_result)) << endl;
+    cout << "EXP(LOG_GAMMA(n+1) + LOG(integral_result))" << TO_STRING(EXP(LOG_GAMMA(n+1) + LOG(integral_result))) << endl;
+    return EXP(LOG_GAMMA(n+1) + LOG(integral_result));
+}
+
+double binomial_process_upper_noncrossing_probability(int n, const vector<double>& upper_bound_steps)
+{
+    cout << "Using translated polynomials!\n";
+    if ((int)upper_bound_steps.size() < n) {
+        throw runtime_error("Binomial process b(t) must cross upper boundary h(t) since h(1) < n and b(t) = n");
+    }
+    PolynomialTranslatedMonomials p(n+1);
+    p.set_multiplicative_coefficient(0, 1.0);
+
+    for (int i = 0; i < (int)upper_bound_steps.size(); ++i) {
+        p.integrate();
+        p.set_additive_coefficient(1, -upper_bound_steps[i]);
+        p.set_multiplicative_coefficient(0, -p.evaluate(upper_bound_steps[i]));
+    }
+
+    FLOAT integral_result = p.evaluate(1);
+    cout << "Integral result: " << TO_STRING(integral_result) << endl;
+    cout << "LOG_GAMMA(n+1) == " << TO_STRING(LOG_GAMMA(n+1)) << endl;
+    cout << "LOG(integral_result) == " << TO_STRING(LOG(integral_result)) << endl;
+    cout << "EXP(LOG_GAMMA(n+1) + LOG(integral_result))" << TO_STRING(EXP(LOG_GAMMA(n+1) + LOG(integral_result))) << endl;
+    return EXP(LOG_GAMMA(n+1) + LOG(integral_result));
+}
+
+//Plain polynomials:
+//1
+//x-L0
+//1/2 x^2 - L0 x - (1/2 L1^2 - L0L1)
+//1/6 x^3 - 1/2 L0 x^2 - (1/2 L1^2 - L0L1)x - 1/6 L2^3 + 1/2 L0 L2^2 + 1/2 L1^2L2 - L0L1L2
+//
+//
+//Translated polynomials:
+//1
+//(x-L0) 
+//1/2 (x-L0)^2 + 0*(x-L1) - (1/2 (L1-L0)^2) = 1/2 x^2 - L0 x - 1/2 L1^2  + L0L1
+
+
 
 double binomial_process_lower_noncrossing_probability(int n, const vector<double>& lower_bound_steps)
 {
