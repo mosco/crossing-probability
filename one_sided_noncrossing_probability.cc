2be88b3b5c929e5881d983b0499a389ed7b9f4f3
Amit Moscovich Eiger <moscovich@gmail.com>
2015-03-04 05:05:30 +0200

    Add exponent-fix and long double support to binomial_process_upper_noncrossing_probability.
    - The exponent-fix is necessary in order to go above n ~= 180 or so, since
      the coefficients of the translated polynomial become much smaller than
      the smallest representable double precision floating point number.
    
    - "long double" allows us to go up to about n ~= 50,000 with minimal performance penalty.

--- one_sided_noncrossing_probability.cc
+++ one_sided_noncrossing_probability.cc
@@ -3,13 +3,14 @@
 #include <cassert>
 #include <iostream>
 #include <sstream>
+
 #include "one_sided_noncrossing_probability.hh"
 
 using namespace std;
 
-#define USE_FLOAT128
+#define FLOAT_PRECISION_BITS 64
 
-#ifdef USE_FLOAT128
+#if FLOAT_PRECISION_BITS == 128
     typedef __float128 FLOAT;
     #include <quadmath.h>
     #define EXP expq
@@ -17,6 +18,9 @@ using namespace std;
     #define GAMMA tgammaq
     #define LOG_GAMMA lgammaq
     #define POW powq
+    #define FREXP frexpq
+    #define LDEXP ldexpq
+    #define MAX_EXP FLT128_MAX_EXP
     #define TO_STRING float128_to_string
     string float128_to_string(__float128 x)
     {
@@ -24,21 +28,48 @@ using namespace std;
         quadmath_snprintf(s, sizeof(s), "%.30Qg", x);
         return string(s);
     }
-#else
+#elif FLOAT_PRECISION_BITS == 80
+    typedef long double FLOAT;
+    #include <cmath>
+    #include <cfloat>
+    #define EXP expl
+    #define LOG logl
+    #define GAMMA tgammal
+    #define LOG_GAMMA lgammal
+    #define POW powl
+    #define FREXP frexpl
+    #define LDEXP ldexpl
+    #define MAX_EXP LDBL_MAX_EXP
+    #define TO_STRING long_double_to_string
+#elif FLOAT_PRECISION_BITS == 64
     typedef double FLOAT;
-    #include <math.h>
+    #include <cmath>
+    #include <cfloat>
     #define EXP exp
     #define LOG log
     #define GAMMA tgamma
     #define LOG_GAMMA lgamma
     #define POW pow
+    #define FREXP frexp
+    #define LDEXP ldexp
+    #define MAX_EXP DBL_MAX_EXP
     #define TO_STRING double_to_string
+#else
+    #error FLOAT_PRECISION_BITS must be 64, 80 or 128.
 #endif
 
 string double_to_string(double x)
 {
     stringstream ss;
     ss.precision(17); // http://stackoverflow.com/questions/554063/how-do-i-print-a-double-value-with-full-precision-using-cout
+    ss << x;
+    return ss.str();
+}
+
+string long_double_to_string(long double x)
+{
+    stringstream ss;
+    ss.precision(22); // http://stackoverflow.com/questions/554063/how-do-i-print-a-double-value-with-full-precision-using-cout
     ss << x;
     return ss.str();
 }
@@ -91,10 +122,12 @@ void Polynomial::integrate()
 class PolynomialTranslatedMonomials {
 public:
     PolynomialTranslatedMonomials(int max_degree);
+    FLOAT get_multiplicative_coefficient(int degree) const;
     void set_multiplicative_coefficient(int degree, FLOAT multiplicative_coefficient);
     void set_additive_coefficient(int degree, FLOAT additive_coefficient);
     FLOAT evaluate(FLOAT x) const;
     void integrate();
+    void ldexp_all_multiplicative_coefficients(int exp);
     friend ostream& operator<<(ostream& stream, const PolynomialTranslatedMonomials& poly);
     vector<FLOAT> multiplicative_coefficients;
     vector<FLOAT> additive_coefficients;
@@ -107,6 +140,13 @@ PolynomialTranslatedMonomials::PolynomialTranslatedMonomials(int max_degree) :
     degree(0)
 {
     assert(max_degree >= 0);
+}
+
+FLOAT PolynomialTranslatedMonomials::get_multiplicative_coefficient(int degree) const
+{
+    assert(degree >= 0);
+    assert(degree <= this->degree);
+    return multiplicative_coefficients[degree];
 }
 
 void PolynomialTranslatedMonomials::set_multiplicative_coefficient(int degree, FLOAT multiplicative_coefficient)
@@ -145,6 +185,13 @@ void PolynomialTranslatedMonomials::integrate()
     additive_coefficients[0] = 0.0;
     multiplicative_coefficients[0] = 0.0;
     ++degree;
+}
+
+void PolynomialTranslatedMonomials::ldexp_all_multiplicative_coefficients(int exp)
+{
+    for (int i = 0; i < degree+1; ++i) {
+        multiplicative_coefficients[i] = LDEXP(multiplicative_coefficients[i], exp);
+    }
 }
 
 static void print_FLOAT_array(const FLOAT* arr, int n)
@@ -243,9 +290,9 @@ double binomial_process_upper_noncrossing_probability_lazy_integration(int n, co
     return EXP(LOG_GAMMA(n+1) + LOG(integral_result));
 }
 
-double binomial_process_upper_noncrossing_probability(int n, const vector<double>& upper_bound_steps)
+double binomial_process_upper_noncrossing_probability_trans(int n, const vector<double>& upper_bound_steps)
 {
-    cout << "Using translated polynomials!\n";
+    cout << "Using translated polynomials! precision: " << FLOAT_PRECISION_BITS << endl;
     if ((int)upper_bound_steps.size() < n) {
         throw runtime_error("Binomial process b(t) must cross upper boundary h(t) since h(1) < n and b(t) = n");
     }
@@ -253,6 +300,7 @@ double binomial_process_upper_noncrossing_probability(int n, const vector<double
     p.set_multiplicative_coefficient(0, 1.0);
 
     for (int i = 0; i < (int)upper_bound_steps.size(); ++i) {
+        cout << "============= i == " << i << ": " << p << endl;
         p.integrate();
         p.set_additive_coefficient(1, -upper_bound_steps[i]);
         p.set_multiplicative_coefficient(0, -p.evaluate(upper_bound_steps[i]));
@@ -264,6 +312,63 @@ double binomial_process_upper_noncrossing_probability(int n, const vector<double
     cout << "LOG(integral_result) == " << TO_STRING(LOG(integral_result)) << endl;
     cout << "EXP(LOG_GAMMA(n+1) + LOG(integral_result))" << TO_STRING(EXP(LOG_GAMMA(n+1) + LOG(integral_result))) << endl;
     return EXP(LOG_GAMMA(n+1) + LOG(integral_result));
+}
+
+FLOAT maximum_multiplicative_coefficient(const PolynomialTranslatedMonomials& p)
+{
+    assert(p.degree >= 1);
+    FLOAT m = p.get_multiplicative_coefficient(0);
+    for (int i = 0; i < p.degree+1; ++i) {
+        FLOAT x = p.get_multiplicative_coefficient(i);
+        if (x > m) {
+            m = x;
+        }
+    }
+    return m;
+}
+
+int extract_exponent(FLOAT x)
+{
+    static int exponent;
+    FREXP(x, &exponent);
+    return exponent;
+}
+
+double binomial_process_upper_noncrossing_probability(int n, const vector<double>& upper_bound_steps)
+{
+    cout << "Using translated polynomials! precision: " << FLOAT_PRECISION_BITS << endl;
+    if ((int)upper_bound_steps.size() < n) {
+        throw runtime_error("Binomial process b(t) must cross upper boundary h(t) since h(1) < n and b(t) = n");
+    }
+    PolynomialTranslatedMonomials p(n+1);
+    p.set_multiplicative_coefficient(0, 1.0);
+
+    int total_exponent_delta = 0;
+    for (int i = 0; i < (int)upper_bound_steps.size(); ++i) {
+        //cout << "============= i == " << i << ": " << p << endl;
+        p.integrate();
+        p.set_additive_coefficient(1, -upper_bound_steps[i]);
+        p.set_multiplicative_coefficient(0, -p.evaluate(upper_bound_steps[i]));
+
+        //cout << "Before expfix: " << p << endl;
+
+        FLOAT max_coef = maximum_multiplicative_coefficient(p);
+        int highest_exponent = extract_exponent(max_coef);
+        if (highest_exponent < 0) {
+            //cout << "highest exponent: " << highest_exponent << endl;
+            p.ldexp_all_multiplicative_coefficients(0 - highest_exponent);
+            total_exponent_delta += 0 - highest_exponent;
+        }
+    }
+    FLOAT integral_result = p.evaluate(1);
+    FLOAT log_integral_result = LOG(integral_result);
+    cout << "Integral result: (no exponent fix) " << TO_STRING(integral_result) << endl;
+    cout << "Exponent delta: " << total_exponent_delta << endl;
+    cout << "LOG_GAMMA(n+1) == " << TO_STRING(LOG_GAMMA(n+1)) << endl;
+    cout << "log_integral_result (no exponent fix) == " << TO_STRING(log_integral_result) << endl;
+    FLOAT noncrossing_probability = EXP(LOG_GAMMA(n+1) + log_integral_result - total_exponent_delta*LOG(2));
+    cout << "Final result: " << TO_STRING(noncrossing_probability) << endl;
+    return noncrossing_probability;
 }
 
 //Plain polynomials:
