1cd558043f3a9b7114baacbd9feaa290e678fd50
Amit Moscovich <amit@moscovich.org>
2020-08-29 23:59:00 -0400

    Bug fix: PoissonPMFGenerator::compute_array now supports lambda==0

--- src/poisson_pmf.cc
+++ src/poisson_pmf.cc
@@ -40,23 +40,25 @@ double PoissonPMFGenerator::evaluate_pmf(double lambda, int k) const
     return exp(-lambda + k*log(lambda) - log_gamma_LUT[k+1]);
 }
 
-int PoissonPMFGenerator::compute_array(int k, double lambda)
+void PoissonPMFGenerator::compute_array(int k, double lambda)
 {
+    assert(k >= 0);
     assert(k <= max_k);
-    if (lambda <= 0) {
+
+    if (lambda < 0) {
             throw runtime_error("Expecting lambda>0 in PoissonPMFGenerator::compute_array()");
+    }
+    if (lambda == 0) {
+        pmf_array_ptr[0] = 1;
+        for (int i = 1; i < k+1; ++i) {
+            pmf_array_ptr[i] = 0;
+        }
+        return;
     }
 
     double log_lambda = log(lambda);
-    double prob_poisson_eq_i;
-    int support = 0;
     for (int i = 0; i < k+1; ++i) {
-        prob_poisson_eq_i = exp(-lambda + i*log_lambda - log_gamma_LUT[i+1]);
-        pmf_array_ptr[i] = prob_poisson_eq_i;
-        if (prob_poisson_eq_i > 0) {
-            support = i+1;
-        }
+        pmf_array_ptr[i] = exp(-lambda + i*log_lambda - log_gamma_LUT[i+1]);
     }
-    return support;
 }
 
--- src/poisson_pmf.hh
+++ src/poisson_pmf.hh
@@ -14,9 +14,9 @@ inline double poisson_pmf(double lambda, int k)
     if (lambda == 0.0) {
         return k == 0 ? 1.0 : 0.0;
     }
-    double log_pmf = -lambda + k*std::log(lambda) - std::lgamma(k+1);
-    return std::exp(log_pmf);
+    return std::exp(-lambda + k*std::log(lambda) - std::lgamma(k+1));
 }
+
 
 class PoissonPMFGenerator {
 public:
@@ -25,9 +25,9 @@ public:
     double evaluate_pmf(double lambda, int k) const;
     // Fills pmf_array_ptr with the PMF of a Poisson random variable:
     //     Pr[Pois(lambda) = 0], ..., Pr[Pois(lambda) = k]
-    // Returns the integer N such that for all indices i >= N we have that due to double-precision rounding
+    // Returns the smallest integer N such that for all indices i >= N we have that due to double-precision rounding
     //     Pr[Pois(lambda) = i] = 0
-    int compute_array(int k, double lambda); 
+    void compute_array(int k, double lambda); 
     const double* get_array() const {return pmf_array_ptr;}
 private:
     int max_k;
